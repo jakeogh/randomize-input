6c7b752d544724669be685740c2817de0658884a
jakeogh <github.com@v6y.net>
2020-07-07 00:07:19 -0700

    auto-commit

---
+++ randompipe/randompipe.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+# pylint: disable=C0111     # docstrings are always outdated and wrong
+# pylint: disable=W0511     # todo is encouraged
+# pylint: disable=R0902     # too many instance attributes
+# pylint: disable=C0302     # too many lines in module
+# pylint: disable=C0103     # single letter var names
+# pylint: disable=R0911     # too many return statements
+# pylint: disable=R0912     # too many branches
+# pylint: disable=R0915     # too many statements
+# pylint: disable=R0913     # too many arguments
+# pylint: disable=R1702     # too many nested blocks
+# pylint: disable=R0914     # too many local variables
+# pylint: disable=R0903     # too few public methods
+# pylint: disable=E1101     # no member for base
+# pylint: disable=W0201     # attribute defined outside __init__
+## pylint: disable=W0703     # catching too general exception
+
+import os
+import sys
+import click
+from pathlib import Path
+from shutil import get_terminal_size
+from icecream import ic
+from kcl.configops import click_read_config
+from kcl.configops import click_write_config_entry
+from kcl.inputops import input_iterator
+from kcl.iterops import randomize_iterator
+
+ic.configureOutput(includeContext=True)
+ic.lineWrapWidth, _ = get_terminal_size((80, 20))
+# import IPython; IPython.embed()
+# import pdb; pdb.set_trace()
+# from pudb import set_trace; set_trace(paused=False)
+
+APP_NAME = 'randompipe'
+
+
+# DONT CHANGE FUNC NAME
+@click.command()
+@click.option('--verbose', is_flag=True)
+@click.option('--debug', is_flag=True)
+@click.option("--null", is_flag=True)
+def cli(verbose, debug, null):
+
+    byte = b'\n'
+    if null:
+        byte = b'\x00'
+
+    config, config_mtime = click_read_config(click_instance=click,
+                                             app_name=APP_NAME,
+                                             verbose=verbose)
+    if verbose:
+        ic(config, config_mtime)
+
+    for item in randomize_iterator(input_iterator(null=null, verbose=verbose), verbose=verbose):
+        print(item, end=byte.decode('utf8'))
+

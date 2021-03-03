1de0f708258c077a7716da13ef45c2aac8fc1994
jakeogh <github.com@v6y.net>
2021-03-03 14:29:05 -0700

    auto-commit

--- randomize_input/randomize_input.py
+++ randomize_input/randomize_input.py
@@ -16,15 +16,119 @@
 # pylint: disable=W0201     # attribute defined outside __init__
 ## pylint: disable=W0703     # catching too general exception
 
+import secrets
+import sys
+import time
+
 import click
-from icecream import ic
 from kcl.iterops import input_iterator
-from kcl.iterops import randomize_iterator
+
+
+def eprint(*args, **kwargs):
+    if 'file' in kwargs.keys():
+        kwargs.pop('file')
+    print(*args, file=sys.stderr, **kwargs)
+
+
+try:
+    from icecream import ic  # https://github.com/gruns/icecream
+except ImportError:
+    ic = eprint
+
+
+
 
 #ic.configureOutput(includeContext=True)
 # import IPython; IPython.embed()
 # import pdb; pdb.set_trace()
 # from pudb import set_trace; set_trace(paused=False)
+
+def append_to_set(*,
+                  iterator,
+                  the_set,
+                  max_wait_time,
+                  min_pool_size,  # the_set always has 1 item
+                  verbose=False,
+                  debug=False):
+
+    assert max_wait_time > 0.01
+    assert min_pool_size >= 2
+
+    time_loops = 0
+    eprint("\nWaiting for min_pool_size: {}\n".format(min_pool_size))
+    while len(the_set) < min_pool_size:
+        start_time = time.time()
+        while (time.time() - start_time) < max_wait_time:
+            time_loops += 1
+            try:
+                the_set.add(next(iterator))
+            except StopIteration:
+                pass
+
+        if time_loops > 1:
+            eprint("\nWarning: min_pool_size: {} was not reached in max_wait_time: {}s so actual wait time was: {}x {}s\n".format(min_pool_size, max_wait_time, time_loops, max_wait_time * time_loops))
+
+        if len(the_set) < min_pool_size:
+            eprint("\nlen(the_set) is {} waiting for min_pool_size: {}\n".format(len(the_set), min_pool_size))
+
+    assert time_loops > 0
+    return the_set
+
+
+
+
+# add time-like memory limit
+# the longer the max_wait, the larger buffer_set will be,
+# resulting in better mixing
+def randomize_iterator(iterator, *,
+                       min_pool_size,
+                       max_wait_time,
+                       buffer_set=None,
+                       verbose=False,
+                       debug=False):
+
+    assert max_wait_time
+    assert min_pool_size
+
+    if min_pool_size < 2:
+        min_pool_size = 2
+
+    if not buffer_set:
+        buffer_set = set()
+        try:
+            buffer_set.add(next(iterator))
+        except StopIteration:
+            pass
+
+    buffer_set = append_to_set(iterator=iterator,
+                               the_set=buffer_set,
+                               min_pool_size=min_pool_size,
+                               max_wait_time=max_wait_time,
+                               verbose=verbose,
+                               debug=debug)
+
+    while buffer_set:
+        try:
+            buffer_set.add(next(iterator))
+            buffer_set.add(next(iterator))
+        except StopIteration:
+            pass
+
+        buffer_set_length = len(buffer_set)
+        random_index = secrets.randbelow(buffer_set_length)
+        next_item = list(buffer_set).pop(random_index)
+        buffer_set.remove(next_item)
+        if debug:
+            eprint("Chose 1 item out of", buffer_set_length)
+
+        if debug:
+            eprint("len(buffer_set):", buffer_set_length - 1)
+
+        if verbose:
+            ic(len(buffer_set), random_index, next_item)
+
+        yield next_item
+
 
 
 @click.command()
